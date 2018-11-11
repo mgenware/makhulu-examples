3fa159791de0b08820a54b8ebd82345a493212f1
Mgen <m@mgenware.com>
2018-11-11 18:14:29 +0800

    Fix uglify example issues

---
+++ src/uglify-js-files/README.md
@@ -0,0 +1,4 @@
+## Run this example
+
+* Navigate to the project root dir
+* `yarn start -b src/uglify-js-files && node ./dist/uglify-js-files`
--- src/uglify-js-files/index.ts
+++ src/uglify-js-files/index.ts
@@ -1,8 +1,9 @@
-import * as mk from '../../../makhulu';
+import * as mk from 'makhulu';
 import { minify } from 'uglify-js';
 
 (async () => {
   const files = await mk.fs.src('./test_files/', '**/*.js');
+  await files.forEach('Source files', mk.fs.printsSrcPath);
   await files.map('Read files', mk.fs.fileToContentString);
   await files.map('Uglify', async data => {
     const content = data.get(mk.fs.FileContent) as string;
@@ -13,4 +14,5 @@ import { minify } from 'uglify-js';
     return data.set(mk.fs.FileContent, uglifyRes.code);
   });
   await files.map('Write files', mk.fs.saveToDirectory('./dist_files/uglify-js-files'));
+  await files.forEach('Dest files', mk.fs.printsDestPath);
 })();
