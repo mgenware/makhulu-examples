3655755fc11f028e4e88eaf356cf4ea43a3d8011
Mgen <m@mgenware.com>
2018-11-17 03:21:43 +0800

    Add html minify example

---
+++ src/minify-html/index.ts
@@ -0,0 +1,21 @@
+/**
+
+* Assuming you have installed the following packages:
+ * makhulu, ky
+ */
+import { minify } from 'html-minifier';
+import * as mk from 'makhulu';
+import * as nodepath from 'path';
+
+(async () => {
+  const srcDir = './test_files/';
+
+  const files = await mk.fs.src(srcDir, '**/*.html');
+  await files.map('Read files', mk.fs.readToString);
+  await files.map('Minify files', async d => {
+    const content = d.get(mk.fs.FileContent) as string;
+    const res = minify(content, { collapseWhitespace: true });
+    return d.set(mk.fs.FileContent, res);
+  });
+  await files.map('Saving to files', mk.fs.writeToDirectory(`./dist_files/${nodepath.basename(__dirname)}`));
+})();
